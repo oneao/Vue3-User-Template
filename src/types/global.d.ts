e195fbcc5592bc2f7d1c872f5798082b990c9a91
oneao <hi_itao@163.com>
2025-03-12 10:28:03 +0800

    c

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,23 +1,29 @@
-interface Window {
-  /** NProgress instance */
-  NProgress?: import("nprogress").NProgress;
-  // $message?: import("@arco-design/web-vue/es/Message/interface").MessageMethod;
+import { AdminStatusEnum } from "@/types/enums"
+
+declare global {
+  interface Window {
+    /** NProgress instance */
+    NProgress?: import("nprogress").NProgress
+    // $message?: import("@arco-design/web-vue/es/Message/interface").MessageMethod;
+  }
+
+  /** Build time of the project */
+  const BUILD_TIME: string
+
+  type IUserInfo = {
+    id: string
+    account?: string
+    email?: string
+    name?: string
+    token?: string
+    avatar?: string
+  }
+
+  type IResData<T = unknown> = {
+    code: number
+    message: string
+    data: T
+  }
 }
 
-/** Build time of the project */
-declare const BUILD_TIME: string;
-
-type IUserInfo = {
-  id: string;
-  account?: string;
-  email?: string;
-  name?: string;
-  token?: string;
-  avatar?: string;
-};
-
-type IResData<T = unknown> = {
-  code: number;
-  message: string;
-  data: T;
-};
+export {} // 确保 TypeScript 将此文件视为模块，防止全局污染
